3853afbd3230f0462a3faf9068f9f7dc5bcb72da
darklordace <firdaus.alghifari@gmail.com>
2019-01-09 01:33:52 +0700

    Yeay 0.11

--- controllers/register.js
+++ controllers/register.js
@@ -46,13 +46,13 @@ const handleRegister = (db, bcrypt) => async (req, resp) => {
 const checkData = (name, email, password) => {
 	let error = '';
 	if (name === '')
-		error += 'Invalid Name<br/>';
+		error += 'Invalid Name\n';
 	if (!(0 < email.lastIndexOf('@') && email.lastIndexOf('@') + 1 < email.lastIndexOf('.') && email.lastIndexOf('.') + 1 < email.length))
-		error += 'Invalid Email Address<br/>';
+		error += 'Invalid Email Address\n';
 	if (password.length < 10 || password.length > 30)
-		error += 'Invalid Password<br/>';
+		error += 'Invalid Password\n';
 	if (error !== '')
-		throw new Error(error.slice(0, error.length - 5));
+		throw new Error(error.slice(0, error.length - 1));
 }
 
 module.exports = {
