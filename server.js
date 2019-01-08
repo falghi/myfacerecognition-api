0c134dc44b4990196753f4240dbcf7f2f2c1c375
darklordace <firdaus.alghifari@gmail.com>
2019-01-08 19:28:44 +0700

    Yeay 0.6

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ const image = require('./controllers/image');
 const db = knex({
 	client: 'pg',
 	connection: {
-		host : 'ec2-54-225-121-235.compute-1.amazonaws.com',
-		user : 'aqyxabavovjtpt',
-		password : 'aea2507907763fb36c0c9189e54a0fc494daa1899cc10abccc78fe3ecc0efe39',
-		database : 'd9n4orc14nlao4'
+		connectionString: process.env.DATABASE_URL,
+		ssl: true,
 	}
 });
 
